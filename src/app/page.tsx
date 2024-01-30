a17c4e013f49b502dd9de09456ec1b27aa4aa155
Ronald <ronaldzamoradev@gmail.com>
2024-01-30 12:18:09 -0500

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 import { useRouter, useSearchParams } from 'next/navigation'
-import { useEffect, useState } from 'react'
+import { useEffect } from 'react'
 import { useData } from '@/store'
 import { useSupabase } from './providers'
 import { Switch } from '@nextui-org/react'
@@ -11,8 +11,6 @@ export default function Home () {
   const { supabase } = useSupabase()
   const loginCode = useSearchParams().get('code')
   const router = useRouter()
-
-  const [coords, setCoords] = useState<any>(null)
 
   const setDeliveryState = () => {
     supabase
@@ -26,18 +24,6 @@ export default function Home () {
         }
       })
   }
-
-  useEffect(() => {
-    if (!('geolocation' in navigator)) {
-      console.log('La geolocalización no está disponible en tu navegador.')
-      return
-    }
-    navigator.geolocation.getCurrentPosition(({ coords }) => {
-      // console.log(`Latitud: ${coords.latitude}`)
-      // console.log(`Longitud: ${coords.longitude}`)
-      setCoords(coords)
-    }, error => console.log(error), { enableHighAccuracy: true })
-  }, [])
 
   useEffect(() => {
     if (loginCode) {
@@ -61,7 +47,6 @@ export default function Home () {
 
   return (
     <main className='flex flex-col gap-4 justify-center items-center'>
-      {coords && (<p>{`latitude: ${coords.latitude} longitud: ${coords.longitude}`}</p>)}
       <div className='w-full flex justify-center gap-48'>
         <p>{active ? 'estas conectado' : 'no estas conectado'}</p>
         <Switch
