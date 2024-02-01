2497604f41da5e58314f571d55be348027751403
Ronald <ronaldzamoradev@gmail.com>
2024-02-01 10:08:56 -0500

    aaaa

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 import { useRouter, useSearchParams } from 'next/navigation'
-import { useEffect, useState } from 'react'
+import { useEffect } from 'react'
 import { useData } from '@/store'
 import { useSupabase } from './providers'
 import { Switch } from '@nextui-org/react'
