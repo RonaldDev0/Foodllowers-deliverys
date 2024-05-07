8a7655dad360cd595d6aa125eaeb64d8bc340754
Ronald <ronaldzamoradev@gmail.com>
2024-05-07 16:00:08 -0500

    feat: update /dasboard with real data

--- src/app/dashboard/History.tsx
+++ src/app/dashboard/History.tsx
@@ -1,12 +1,48 @@
+'use client'
+import { useState, useEffect } from 'react'
+import { useData } from '@/store'
+import { useSupabase } from '../providers'
 import { HistoryCard } from './HistoryCard'
-import { data } from './data'
+
+export interface Item {
+  id: string
+  created_at: string
+  amount: number
+}
 
 export function History () {
+  const { deliveryId } = useData()
+  const { supabase } = useSupabase()
+
+  const [history, setHistory] = useState<Item[]>([])
+
+  useEffect(() => {
+    if (!deliveryId) {
+      return
+    }
+
+    supabase
+      .from('transactions')
+      .select('*')
+      .eq('delivery_id', deliveryId)
+      .order('created_at', { ascending: false })
+      .range(0, 10)
+      .then(({ data, error }) => {
+        if (error) {
+          return
+        }
+        console.log(data)
+        setHistory(data)
+      })
+  }, [deliveryId])
+
   return (
     <div className='w-96 flex flex-col gap-2'>
       <p>Detalle de ganancia por dia</p>
-      <div className='max-h-[74vh] overflow-auto'>
-        {data.map(item => <HistoryCard key={item.id} item={item} />)}
+      <div className='max-h-[70vh] overflow-auto'>
+        {history.map(item => (
+          <HistoryCard key={item.id} item={item} />
+        ))}
       </div>
     </div>
   )
--- src/app/dashboard/HistoryCard.tsx
+++ src/app/dashboard/HistoryCard.tsx
@@ -1,19 +1,20 @@
 'use client'
 import { Card, CardBody, Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, useDisclosure, Button } from '@nextui-org/react'
 import { ArrowRight } from 'lucide-react'
+import type { Item } from './History'
 
-export function HistoryCard ({ item }: any) {
+export function HistoryCard ({ item }: { item: Item }) {
   const { isOpen, onOpen, onOpenChange } = useDisclosure()
 
   return (
     <>
-      <Card className='w-full hover:bg-purple-950 transition-all cursor-pointer h-14 my-2'>
-        <CardBody onClick={onOpen}>
-          <div className='flex justify-between'>
-            <p>{item.date}</p>
+      <Card className='w-full cursor-pointer h-14 my-2'>
+        <CardBody onClick={onOpen} className='group hover:group-hover'>
+          <div className='flex justify-between items-center'>
+            <p>{new Date(item.created_at).toLocaleDateString('es-ES')}</p>
             <div className='flex gap-4'>
-              <p>$ {item.earnings.toLocaleString()}</p>
-              <ArrowRight size={28} />
+              <p>{item.amount.toLocaleString('es-Es', { style: 'currency', currency: 'COP' })}</p>
+              <ArrowRight size={28} className='opacity-0 group-hover:opacity-100 transition-all' />
             </div>
           </div>
         </CardBody>
@@ -26,7 +27,8 @@ export function HistoryCard ({ item }: any) {
                 Detalles de las ganancias
               </ModalHeader>
               <ModalBody>
-                {JSON.stringify(item, null, 2)}
+                <p>{new Date(item.created_at).toLocaleDateString('es-ES')}</p>
+                <p>{item.amount.toLocaleString('es-Es', { style: 'currency', currency: 'COP' })}</p>
               </ModalBody>
               <ModalFooter>
                 <Button color='secondary' onPress={onClose}>
